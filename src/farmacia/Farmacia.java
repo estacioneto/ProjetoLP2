fe5418e548214084ce56107ee11703ad10a29d7f
Thaynan Andrey <thaynan.nunes@ccc.ufcg.edu.br>
2016-04-28 17:27:19 -0300

    Adicao de novos testes ao MedicamentoTest

--- src/farmacia/Farmacia.java
+++ src/farmacia/Farmacia.java
@@ -1,5 +1,31 @@
 package farmacia;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Farmacia {
 
+	private List<Medicamento> listaMedicamentos;
+	private MedicamentoFactory medicamentoFactory;
+	
+	public Farmacia(){
+		this.listaMedicamentos = new ArrayList<>();
+		this.medicamentoFactory = new MedicamentoFactory();
+	}
+
+	public List<Medicamento> getListaMedicamentos() {
+		List<Medicamento> copiaDaLista = new ArrayList<>(this.listaMedicamentos);
+		return copiaDaLista;
+	}
+	
+	public boolean addMedicamento(String nome, double preco, int quantidade, String tipoMedicamento) throws Exception{
+		Medicamento medicamento = medicamentoFactory.criaMedicamento(nome, preco, quantidade, tipoMedicamento);
+		boolean condicaoDeAdicao = this.listaMedicamentos.add(medicamento);
+		return condicaoDeAdicao;
+	}
+	
+	public void ordenaListaMedicamentos(){
+		Collections.sort(this.listaMedicamentos);
+	}
 }
--- src/farmacia/MedicamentoGenerico.java
+++ src/farmacia/MedicamentoGenerico.java
@@ -2,8 +2,14 @@ package farmacia;
 
 public class MedicamentoGenerico extends Medicamento{
 
+	public static double DESCONTO = 0.6;
+	
 	public MedicamentoGenerico(String nome, double preco, int quantidade) throws Exception {
 		super(nome, preco, quantidade);
 	}
 
+	@Override
+	public double calculaPreco(){
+		return super.calculaPreco() * DESCONTO;
+	}
 }
