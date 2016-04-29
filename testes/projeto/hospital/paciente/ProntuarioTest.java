64619de7c9daf1a61ba501ee5d90c102dabbdddf
Eric Breno <eric.santos@ccc.ufcg.edu.br>
2016-04-29 17:24:03 -0300

    Criada classe para teste de prontuario

---
+++ testes/projeto/hospital/paciente/ProntuarioTest.java
@@ -0,0 +1,14 @@
+package projeto.hospital.paciente;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProntuarioTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
